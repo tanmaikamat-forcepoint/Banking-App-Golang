590c9071292939443ce061a61db58d0fc4af2a9b
Tanmai2002_FP_Local <tanmai.kamat@forcepoint.com>
2024-10-24 18:19:35 +0530

    Completed with Env Setup

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.1
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"bankManagement/app"
 	"bankManagement/repository"
 	"bankManagement/utils/log"
+	"os"
 	"sync"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	name := "Bank Management App"
 	log := NewLogger()
-	db := NewDBConnection()
+	db := NewDBConnection(log)
 	if db == nil {
 		log.Error("DB connection Failed")
 	}
@@ -27,9 +31,22 @@ func main() {
 	appObj.StartServer()
 }
 
-func NewDBConnection() *gorm.DB {
-
-	return nil
+func NewDBConnection(log log.WebLogger) *gorm.DB {
+	host, b1 := os.LookupEnv("HOST")
+	user, b2 := os.LookupEnv("USER")
+	password, b3 := os.LookupEnv("PASSWORD")
+	database, b4 := os.LookupEnv("DATABASE")
+	if !(b1 && b2 && b3 && b4) {
+		panic("Error: Enviroment Variables not Initialized")
+	}
+	db, err := gorm.Open("mysql", user+":"+password+"@("+host+")/"+database+"?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Error(err.Error())
+		return nil
+	}
+	log.Info("Database Connected")
+	db.LogMode(true)
+	return db
 }
 
 func NewLogger() log.WebLogger {
